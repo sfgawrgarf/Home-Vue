f8388085edad05f78109f185f9d55144342cd741
sfgawrgarf <93918570+sfgawrgarf@users.noreply.github.com>
2026-04-13 00:33:57 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="background"></div>
     <Home />
   <footer>
-    <span>© 2024 Made in <a href="/" target="_blank">{{ userName }}</a></span>
+    <span>© 2026 Made in <a href="/" target="_blank">{{ userName }}</a></span>
     <a v-if="icpNumber" href="https://beian.miit.gov.cn/" target="_blank">{{ icpNumber }}</a>
     <a v-if="policenumber" :href="`https://beian.mps.gov.cn/#/query/webSearch?police=${policenumber}`" target="_blank" class="police_link">
       <span class="police_img"></span> {{ policenumber }}
